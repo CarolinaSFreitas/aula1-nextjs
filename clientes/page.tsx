af1066ff1902bfd179a0171f1d2cb3d424f7a89c
Carolina Freitas <99994934+CarolinaSFreitas@users.noreply.github.com>
2024-03-07 22:04:14 -0300

    Add files via upload

---
+++ clientes/page.tsx
@@ -0,0 +1,7 @@
+function Produtos(){
+    return(
+        <h1>Oie Galeras</h1>
+    )
+}
+
+export default Produtos
---
+++ detalhes/[id]/page.tsx
@@ -0,0 +1,10 @@
+function Detalhes({ params }: { params: { id: string } }) {
+
+    const id = params.id
+
+    return (
+        <h1 className="text-5xl">Detalhes do Produto: </h1>
+    )
+}
+
+export default Detalhes 
